5053d4e445a309325590afb7bc57960e17fe70c2
Bill <clowd150@gmail.com>
2015-01-19 17:48:29 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,309 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var bcrypt = require('bcryptjs');
+//var csrf = require('csurf');
+var mongoose = require('mongoose');
+var sessions = require('client-sessions');
+
+var url = require('url');
+
+var Schema = mongoose.Schema; //allows use to define our schema
+var ObjectId = Schema.ObjectId;
+
+//CONNECT TO MONGO
+mongoose.connect('mongodb://localhost/auth');
+
+//User is a mongoose model (meaning it represents a user in the database). Then specify a schema, which is how the data is going to be represented in the db. List the fields and what type of value they are. The id is the value that MongoDB provides us.
+var User = mongoose.model('User', new Schema({
+	id: ObjectId,
+	firstName: String,
+	lastName: String,
+	email: { type: String, unique: true },
+	password: String,
+	recipes: [{ recipeName: String, url: String, notes: String }]
+}));
+
+var app = express();
+app.set('view engine', 'ejs');
+app.locals.pretty = true;
+app.use(express.static('public'));
+
+//MIDDLEWARE
+//This middleware takes the body of the http request from the user and make it available via req.body and allow us to access it.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(sessions({
+	cookieName: 'session',
+	secret: "*-dafdpas23rsda232222;;al;",
+	duration: 30 * 60 * 1000,
+	activeDuration: 5 * 60 * 1000
+}));
+
+//app.use(csrf());
+
+app.use(function(req, res, next) {
+	if (req.session && req.session.user) {
+		User.findOne({ email: req.session.user.email }, function(err, user) {
+			if (user) {
+				req.user = user;
+				delete req.user.password;
+				req.session.user = user;
+				res.locals.user = user;
+			}
+			next();
+		});
+	} else {
+		next();
+	}
+});
+
+function requireLogin(req, res, next) {
+	if (!req.user) {
+		res.redirect('/login');
+	} else {
+		next();
+	}
+}
+
+app.get('/', function(req, res) {
+	res.render('index.ejs');
+});
+
+app.get('/register', function(req, res) {
+	res.render('register.ejs'/*, { csrfToken: req.csrfToken() } */);
+});
+
+app.post('/register', function(req, res) {
+	var hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+	var user = new User({
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		password: hash
+	});
+	user.save(function(err) {
+		if (err) {
+			var err = 'Something bad happened! Try again!';
+			if (err.code === 11000) { //this is the error mongoDB returns if something's nonunique
+				err = 'That email is already taken, try another.';
+			}
+
+			res.render('register.ejs', { error: error });
+		} else {
+			res.redirect('/dashboard');
+		}
+	});
+});
+
+app.get('/login', function(req, res) {
+	res.render('login.ejs'/*, { csrfToken: req.csrfToken() } */);
+});
+
+app.post('/login', function(req, res) {
+	User.findOne({ email: req.body.email }, function(err, user) {
+		if (!user) {
+			res.render('login.ejs', { error: 'Invalid email or password.'});
+		} else {
+			if (bcrypt.compareSync(req.body.password, user.password)) {
+				req.session.user = user; //set-cookie: session={email: ..., password: ..., ..}
+				res.redirect('/dashboard');
+			} else {
+				res.render('login.ejs', { error: 'Invalid email or password.'});				
+			}
+		}
+	});
+});
+
+app.get('/dashboard', requireLogin, function(req, res) {
+	res.render('dashboard.ejs');
+});
+
+app.get('/profile', requireLogin, function(req, res) {
+	res.render('profile.ejs'/*, { csrfToken: req.csrfToken() } */);
+	//console.log(req.session.user.email);
+	//console.log(req.session.user);
+});
+
+app.get('/recipelist', requireLogin, function(req, res) {
+	console.log("DDF");
+	res.render('recipelist.ejs');
+});
+
+// POST RECIPE
+app.post('/profile', function(req, res) {
+	User.findOne({ email: req.session.user.email }, function(err, user) {
+		var formattedUrl = formatUrl(req);
+		user.recipes.push({ recipeName: req.body.recipe, url: formattedUrl, notes: req.body.notes });
+		//user.recipes.push(req.body.recipe);
+		//user.urls.push(req.body.url);
+		user.save(function(err, thor) {
+		  if (err) return console.error(err);
+		  console.dir(thor);
+		});
+		res.redirect('/profile');
+	});
+});
+
+// UPDATE NAME
+app.post('/updateName/:recordID', function(req, res) {
+	User.findOne({ email: req.session.user.email }, function(err, user) {
+		if (err) return console.error(err);
+		var result = user.recipes.filter(function( obj ) {
+			return (obj._id == req.params.recordID);
+		});
+		var y = user.recipes.indexOf(result[0]);
+		user.recipes[y].recipeName = req.body.newname;
+		user.save(function (err) {
+			if (err) return console.error(err);
+			console.log('the sub-doc was updated')
+		});
+		console.log(user);
+		res.sendStatus(201);
+	});
+});
+
+// UPDATE URL
+app.post('/updateUrl/:recordID', function(req, res) {
+	console.log(req.body.newurl);
+	User.findOne({ email: req.session.user.email }, function(err, user) {
+		if (err) return console.error(err);
+		var result = user.recipes.filter(function( obj ) {
+			return (obj._id == req.params.recordID);
+		});
+		var y = user.recipes.indexOf(result[0]);
+		var formattedUrl = formatUrlUpdate(req);
+		user.recipes[y].url = formattedUrl;
+		user.save(function (err) {
+			if (err) return console.error(err);
+			console.log('the sub-doc was updated')
+		});
+		console.log(user);
+		res.sendStatus(201);
+	});
+});
+
+// UPDATE NOT
+app.post('/updateNote/:recordID', function(req, res) {
+	console.log(req.body.newnote);
+	User.findOne({ email: req.session.user.email }, function(err, user) {
+		if (err) return console.error(err);
+		var result = user.recipes.filter(function( obj ) {
+			return (obj._id == req.params.recordID);
+		});
+		var y = user.recipes.indexOf(result[0]);
+		user.recipes[y].notes = req.body.newnote;
+		user.save(function (err) {
+			if (err) return console.error(err);
+			console.log('the sub-doc was updated')
+		});
+		console.log(user);
+		res.sendStatus(201);
+	});
+});
+
+// DELETE RECIPE ENTRY
+app.get('/profile/delete/:recipeID', function(req, res) {
+	User.findOne({ email: req.session.user.email }, function(err, record) {
+		if (err) return console.error(err);
+		var result = record.recipes.filter(function( obj ) {
+			return (obj._id == req.params.recipeID);
+		});
+		//console.log(result[0]);
+		var y = record.recipes.indexOf(result[0]);
+		var myRemoval = record.recipes[y].remove();
+		record.save(function (err) {
+			if (err) return console.error(err);
+			console.log('the sub-doc was removed')
+		});
+		res.redirect('/profile');
+	});
+});
+
+
+app.get('/profile/sortZA', function(req, res) {
+	User.findOne({ email: req.session.user.email }, function(err, record) {
+		if (err) return console.error(err);
+		record.recipes.sort(function(a, b){
+			var nameA = a.recipeName.toLowerCase(), nameB = b.recipeName.toLowerCase()
+			if (nameA < nameB) //sort string ascending
+			return -1 
+			if (nameA > nameB)
+			return 1
+			return 0 //default return value (no sorting)
+		});
+		console.log(record.recipes);
+		record.save(function (err) {
+			if (err) return console.error(err);
+			console.log('the sub-doc was sorted')
+		});
+
+		res.redirect('/profile');
+	});
+});
+
+app.get('/profile/sortAZ', function(req, res) {
+	User.findOne({ email: req.session.user.email }, function(err, record) {
+		if (err) return console.error(err);
+		record.recipes.sort(function(a, b){
+			var nameA = a.recipeName.toLowerCase(), nameB = b.recipeName.toLowerCase()
+			if (nameB < nameA) //sort string ascending
+			return -1 
+			if (nameB > nameA)
+			return 1
+			return 0 //default return value (no sorting)
+		});
+		console.log(record.recipes);
+		record.save(function (err) {
+			if (err) return console.error(err);
+			console.log('the sub-doc was sorted')
+		});
+
+		res.redirect('/profile');
+	});
+});
+
+
+app.get('/logout', function(req, res) {
+	req.session.reset();
+	res.redirect('/');
+});
+
+
+app.listen(3000, function(req, res) {
+	console.log('App listening on port 3000');
+});
+
+
+function formatUrl(req) {
+	console.log("Default: " + url.parse(req.body.url, true).href);
+	var href = url.parse(req.body.url, true).href;
+	var protocol = url.parse(req.body.url, true).protocol;
+	var path = url.parse(req.body.url, true).path;
+	var formattedUrl;
+	console.log(href);
+	console.log(protocol);
+	console.log(path);
+	console.log("REQUEST LENTGH: " + req.body.url.length);
+	if (!protocol && req.body.url.length >= 1) {
+		formattedUrl = "http://" + href;
+	} else {
+		formattedUrl = href;
+	}
+	return formattedUrl;
+}
+
+function formatUrlUpdate(req) {
+	console.log("Default: " + url.parse(req.body.newurl, true).href);
+	var href = url.parse(req.body.newurl, true).href;
+	var protocol = url.parse(req.body.newurl, true).protocol;
+	var path = url.parse(req.body.newurl, true).path;
+	var formattedUrl;
+	console.log(href);
+	console.log(protocol);
+	console.log(path);
+	if (!protocol && req.body.newurl.length >= 1) {
+		formattedUrl = "http://" + href;
+	} else {
+		formattedUrl = href;
+	}
+	return formattedUrl;
+}
---
+++ public/script.js
@@ -0,0 +1,348 @@
+$(document).ready(function() {
+	// Post Recipe
+	$(document).on('click', '#submitRecipe', function(e) {
+		e.preventDefault();
+		$.ajax({
+		    type: "POST",
+		    url: "/profile",
+		    data: $('#recipeEntry').serialize()
+		}).done(function() {
+			$.get( "/recipelist", function(data) {
+				var response = $(data);
+		    	generatedId = response.find('input').val();
+		    	generatedIdParent = response.find('input').parent();
+				$('.result').html(generatedIdParent);
+				resetNotesFormat();
+			});
+		});
+		$('#recipeEntry')[0].reset();
+	});
+
+	// Hide Notes on load
+	$('.notes').hide();
+
+	// Toggle Menu
+	$(document).on('click', '.edit', function(e) {
+		e.preventDefault();
+		var menu = $(this).parent().prev();
+		menu.toggle();
+
+		$(document).mouseup(function (e) {
+		    var container = menu;
+		    if (!container.is(e.target) // if the target of the click isn't the container...
+		        && container.has(e.target).length === 0) // ... nor a descendant of the container
+		    {
+		        container.hide();
+		    }
+		});
+	});
+
+
+	// Edit Recipe Name
+	$(document).on('click', '.editName', function(e) {
+		e.preventDefault();
+		$('.menu').hide();
+		var currentRecipe = $(this).parent().parent();
+
+		//Don't include var here
+		nameClone = $(this).parent().next().html();
+		console.log(nameClone);
+
+		var name = $(this).parent().next().find('b');
+		var namevalue = name.html();
+		console.log("First: " + namevalue);
+		$('.rName').find('img').unwrap();
+		$('.editImage').replaceWith("<span class='space'>&nbsp;&nbsp;&nbsp;&nbsp;<span>");
+
+		var nameParent = name.parent();
+		nameParent.html("&nbsp;&nbsp;&nbsp;&nbsp;<input id='newname' type='text' name='newname'>");
+		$('#newname').val(namevalue);
+		$('#newname').select();
+
+		var recordID = $(this).parent().prev().val();
+		console.log(recordID);
+
+		$(document).mouseup(function(event){
+		    var target = $("#newname");
+		    if (!target.is(event.target) && target.is(":visible")) {
+		       target.parent().html(nameClone);
+		       $('.space').replaceWith("<a class='edit' href='#'><img src='edit.png' class='editImage'></a>");
+		    }
+		});
+
+		$('#newname').keydown(function (e){
+			if(e.keyCode == 13) {
+				$.ajax({
+			        url: "/updateName/" + recordID,
+			        type: "POST",
+			        data: $('#newname').serialize()
+			    }).done(function() {
+			    	$.get( "/recipelist", function(data) {
+			    		var response = $(data);
+			    		var returnID = response.find("#" + recordID);
+			    		//alert(returnID.val());
+			    		var parentRecipe = returnID.parent().html();
+						currentRecipe.html(parentRecipe);
+						$('.space').replaceWith("<a class='edit' href='#'><img src='edit.png' class='editImage'></a>");
+						resetNotesFormat();
+					});
+			    });
+			}
+		});
+	});
+
+	// Edit URL
+	$(document).on('click', '.editUrl', function(e) {
+		e.preventDefault();
+		var currentRecipe = $(this).parent().parent();
+		$('.menu').hide();
+		$('.rName').find('img').unwrap();
+		$('.editImage').replaceWith("<span class='space'>&nbsp;&nbsp;&nbsp;&nbsp;<span>");
+
+		$('.editImage').hide();
+		var editButtonClone = $(this).parent().next().find('a')//.html();
+		//editButton = $(this).parent().next().find('a');
+		//editButton.replaceWith("<img src='space.png'>");
+		//editButtonClone.hide();
+
+		//Don't include var here
+		urlClone = $(this).parent().next().next().html();
+		urlClone2 =  $.trim(urlClone);
+		console.log(urlClone2);
+		
+		var urlValue = $(this).parent().next().next().text();
+		var urlValue2 = $.trim(urlValue);
+		console.log("Url Value: " + urlValue2);
+
+		var urlParent = $(this).parent().next().next();
+		urlParent.html("<input id='newurl' type='text' name='newurl'>");
+		$('#newurl').val(urlValue2);
+		$('#newurl').select();
+		
+		var recordID = $(this).parent().prev().val();
+		console.log(recordID);
+
+		$(document).mouseup(function(event){
+		    var target = $("#newurl");
+		    if (!target.is(event.target) && target.is(":visible")) {
+		       target.parent().html(urlClone2);
+		       $('.space').replaceWith("<a class='edit' href='#'><img src='edit.png' class='editImage'></a>");
+		    }
+		});
+	
+		$('#newurl').keydown(function (e){
+			if(e.keyCode == 13) {
+				$.ajax({
+			        url: "/updateUrl/" + recordID,
+			        type: "POST",
+			        data: $('#newurl').serialize()
+			    }).done(function() {
+			    	$.get( "/recipelist", function(data) {
+			    		var response = $(data);
+			    		var returnID = response.find("#" + recordID);
+			    		//alert(returnID.val());
+			    		var parentRecipe = returnID.parent().html();
+						currentRecipe.html(parentRecipe);
+						$('.space').replaceWith("<a class='edit' href='#'><img src='edit.png' class='editImage'></a>");
+
+						// RELOAD NOTES CORRECTLY
+						var parentRecipe = returnID.parent();
+						var newFormattedRecipe = $.trim(parentRecipe.find('.notes').html())
+						newFormattedRecipe = newFormattedRecipe.replace(/\n\r?/g, '<br>');
+						currentRecipe.find('.notes').html(newFormattedRecipe);
+					});
+			    });
+			}
+		});
+		
+	});
+	
+	// Edit Notes
+	var noteRecordID;
+	var currentNoteRecipe;
+	var noteClone;
+	$(document).on('click', '.editNotes', function(e) {
+		//var noteRecordID;
+		e.preventDefault();
+		currentNoteRecipe = $(this).parent().parent();
+		$('.menu').hide();
+		$('.rName').find('img').unwrap();
+
+		//Don't include var here
+		noteClone = $(this).parent().next().next().next().next().html();
+		console.info('Note Clone: ' + noteClone +"\nNoteClone length: " + noteClone.length);
+
+
+		if (noteClone.length >= 1) {
+			var noteSection = $(this).parent().next().next().next().next();
+			
+			if (!noteSection.is(":visible")) {
+				noteSection.toggle();
+			}
+
+			$('.editImage').replaceWith("<span class='space'>&nbsp;&nbsp;&nbsp;&nbsp;<span>");
+
+			var noteValue = $(this).parent().next().next().next().next().html();
+			console.log("Note Value: " + noteValue);
+			var noteValue2 = noteValue.replace(/<br>/g, '\r');
+			console.log("Note Value2: " + noteValue2);
+
+			var noteParent = $(this).parent().next().next().next().next();
+			console.log("noteParent: " + noteParent.html());
+			noteParent.html("<textarea id='newnote' name='newnote'></textarea><button id='newNoteSubmit'>Save</button><button id='cancel'>Cancel</button>");
+			$('#newnote').val(noteValue2);
+			
+			noteRecordID = $(this).parent().prev().val();
+			console.log("NOTERECORDID: " + noteRecordID);
+
+		} else {
+			console.warn('ELSE BLCOK');
+			$('.editImage').replaceWith("<span class='space'>&nbsp;&nbsp;&nbsp;&nbsp;<span>");
+			var noteValue = $(this).parent().next().next().next().next().html();
+			var noteParent = $(this).parent().next().next().next();
+			noteParent.html("<textarea id='newnote' name='newnote'></textarea><button id='newNoteSubmit2'>Save</button><button id='cancel'>Cancel</button>");
+			$('#newnote').val(noteValue);
+
+			noteRecordID = $(this).parent().prev().val();
+			console.log("noteRecordID: " + noteRecordID);
+		}
+	});
+
+	// Update existing note
+	$(document).on('click', '#newNoteSubmit', function(e) {
+		e.preventDefault();
+		$.ajax({
+	        url: "/updateNote/" + noteRecordID,
+	        type: "POST",
+	        data: $('#newnote').serialize()
+	    }).done(function() {
+	    	$.get( "/recipelist", function(data) {
+	    		var response = $(data);
+	    		var returnID = response.find("#" + noteRecordID);
+	    		//alert(returnID.val());
+	    		//var parentRecipe = returnID.parent().html();
+				var parentRecipe = returnID.parent();
+				var newFormattedRecipe = $.trim(parentRecipe.find('.notes').html())
+				newFormattedRecipe = newFormattedRecipe.replace(/\n\r?/g, '<br>');
+	    		console.warn(newFormattedRecipe);
+	    		console.info(returnID.val());
+				if (newFormattedRecipe.length >= 1) {
+					currentNoteRecipe.find('.notes').html(newFormattedRecipe);
+				} else {
+					currentNoteRecipe.find('.noteIndicator').remove();
+					currentNoteRecipe.find('.notes').replaceWith("<div class='noNotes'></div>");
+				}
+				$('.space').replaceWith("<a class='edit' href='#'><img src='edit.png' class='editImage'></a>");
+				existingNote = false;
+			});
+	    });
+	});
+
+	// Submit new note
+	$(document).on('click', '#newNoteSubmit2', function(e) {
+		e.preventDefault();
+		$.ajax({
+	        url: "/updateNote/" + noteRecordID,
+	        type: "POST",
+	        data: $('#newnote').serialize()
+	    }).done(function() {
+	    	$.get( "/recipelist", function(data) {
+	    		var response = $(data);
+	    		var returnID = response.find("#" + noteRecordID);
+	    		//alert(returnID.val());
+	    		//var parentRecipe = returnID.parent().html();
+				var parentRecipe = returnID.parent();
+				var newFormattedRecipe = $.trim(parentRecipe.find('.notes').html())
+				newFormattedRecipe = newFormattedRecipe.replace(/\n\r?/g, '<br>');
+	    		console.warn(newFormattedRecipe);
+
+	    		console.log("Current RECIPE: "  + currentNoteRecipe.html());
+				currentNoteRecipe.find('.noNotes').replaceWith("<div class='noteIndicator'><a href='#' class='noteToggler'>Notes</a></div><div class='notes'>" + newFormattedRecipe + "</div>");
+
+				$('.space').replaceWith("<a class='edit' href='#'><img src='edit.png' class='editImage'></a>");
+			});
+	    });
+	});  
+
+	$(document).on('click', '#cancel', function(e) {
+		e.preventDefault();
+		$('.space').replaceWith("<a class='edit' href='#'><img src='edit.png' class='editImage'></a>");
+		var target = $("#newnote").parent();
+		target.html(noteClone);
+	});
+
+	// Delete Recipe
+	$(document).on('click', '.delete', function(e) {
+		e.preventDefault();
+		var currentRecipe = $(this).parent().parent();
+		$('.menu').hide();
+
+		var recordID = $(this).parent().prev().val();
+		console.log(recordID);
+
+		var confirmation = confirm('Are you sure you want to delete this recipe?');
+		if (confirmation) {
+		   $.ajax({
+		        url: "/profile/delete/" + recordID,
+		        type: "GET"
+		    }).done(function() {
+				currentRecipe.remove();
+			});
+		} else {
+			return;
+		}
+	});
+
+
+	// Hover Color
+	$(document).on('mouseenter', '.rName', function() {
+		$(this).animate({"backgroundColor": "#effbfc"}, 120);
+		$(this).next().animate({"backgroundColor": "#effbfc"}, 120);
+	});
+	$(document).on('mouseout', '.rName', function() {
+		$(this).animate({"backgroundColor": "white"}, 170);
+		$(this).next().animate({"backgroundColor": "white"}, 170);
+	});
+
+	// Toggle Notes
+	$(document).on('click', '.noteToggler', function(e) {
+		e.preventDefault();
+		var note = $(this).parent().next();
+		note.toggle();
+	});
+
+
+	// Implement line breaks in notes field
+	resetNotesFormat();
+	function resetNotesFormat() {
+		$(".notes").each(function(index) {
+			var noteText = $.trim($(this).html());
+			var noteTextFormated = noteText.replace(/\n\r?/g, '<br>');
+			$(this).html(noteTextFormated);
+		});
+	}
+
+	// Sort
+	$(document).on('click', '#sortAZ', function(e) {
+		e.preventDefault();
+		$.get( "/profile/sortAZ", function() {
+			
+		}).done(function() {
+			$.get( "/recipelist", function(data) {
+				$('.result').html(data);
+			});
+		});
+	});
+
+	$(document).on('click', '#sortZA', function(e) {
+		e.preventDefault();
+		$.get( "/profile/sortZA", function() {
+			
+		}).done(function() {
+			$.get( "/recipelist", function(data) {
+				$('.result').html(data);
+			});
+		});
+	});
+
+});
