c69c41f79b3aba70a52c756dccd03b54daf7b962
Bill <clowd150@gmail.com>
2015-02-09 19:02:10 -0800

    protected sensative database uri

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ if (!process.env.PORT) {
 	mongoose.connect('mongodb://localhost/auth');
 } else {
 	console.log("APP RUNNING IN HEROKU!!!");
-	console.log("My Uri " + process.env.MONGOLAB_URI);
-	var mongodbUri = "mongodb://heroku_app33846167:olsihqecng2qs1r0ut66tmob28@ds041831.mongolab.com:41831/heroku_app33846167";
+	var mongodbUri = process.env.MONGOLAB_URI; // A Heroku config variable;
 	var mongooseUri = uriUtil.formatMongoose(mongodbUri);
 	mongoose.connect(mongooseUri, options);
 }
